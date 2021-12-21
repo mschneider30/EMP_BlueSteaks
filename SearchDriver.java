f0f393059402257a631e5cb66f7575fe8bb7e9bf
Max Schneider <Max_Schneider@Matthews-iMac.local>
2021-12-20 21:15:33 -0500

    Added all

--- SearchDriver.java
+++ SearchDriver.java
@@ -35,6 +35,9 @@ public class SearchDriver {
 		int[] cien_millon = new int[100_000_000];
 
 		int[][] testcases = {uno, diez, cien, mil, diez_mil, cien_mil, millon, diez_millon, cien_millon};
+		int[][] results = new int[9][3];
+
+		int counter = 0;
 
 		for (int [] r : testcases){
 			populate(r);
@@ -42,27 +45,37 @@ public class SearchDriver {
 			System.out.println("Starting to test array with length: " + r.length);
 			long start0 = System.currentTimeMillis();
 			int x = 0;
-			int d=100_000_000;
+			int d=10_000;
 			while (x < d) {
-				BinSearch.binSearch(r, r.length - 2);
+				BinSearch.binSearch(r, r.length );
 				x++;
 			}
 			long end0 = System.currentTimeMillis();
 			System.out.println("Time taken to binary search " + d + " times: " + (end0 - start0));
 
 			long start1 = System.currentTimeMillis();
-			int y = 99_990_000;
+			int y = 0;
 			while (y < d) {
-				LinSearch.linSearch(r, r.length - 2);
+				LinSearch.linSearch(r, r.length );
 				y++;
 			}
 			long end1 = System.currentTimeMillis();
 			System.out.println("Time taken to linear search " + d + " times: " + (end1 - start1));
 
 			System.out.println("Binary Search was " + ((end1-start1) - (end0 - start0 )) + " ms faster!");
+
+			results[counter][0]=counter;
+			results[counter][1]= (int)(end0 - start0);
+			results[counter][2]= (int)(end1 - start1);
+
+
+			counter+=1;
 		}
 
-
+		System.out.println("Now Printing Data Table of Results...");
+		for (int i = 0; i < counter; i++) {
+			System.out.println("Trial Number: " + results[i][0] + "  Bin Time: " + results[i][1] + "  Lin Time: " + results[i][2]);
+		}
 
 	}
 }
